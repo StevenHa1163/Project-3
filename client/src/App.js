dfa60ecc318b588f0ec8b43aefd75b626511a6a9
John Bynum <iexclusiveproductions@gmail.com>
2022-11-23 17:52:07 -0500

    Working on Front End

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 function App() {
   return (
     <div className="App">
+      <h1>Welcome, Viewer</h1>
+      <h4>Your Tasks</h4>
       
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+
+          <div className="text">Get the bread</div>
+
+          <div className="delete-todo">X</div>
+        </div>
+      </div>
     </div>
   );
 }
