872074acb183c1b710f97bfe3490680543748db0
Steven Ha <stevenhaabc@gmail.com>
2022-11-25 21:04:19 -0500

    Creating delete and complete functions to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
 import { useState, useEffect } from 'react';
 
+const API_BASE = "http://localhost:3001";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+  }, [])
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+      .then(res => res.json())
+      .then(data => setTodos(data))
+      .then(err => console.error("Error: ", err))
+  }
+
+  const completeTodo = async id =>{
+      const data = await fetch(API_BASE + "/todo/complete/" + id)
+        .then(res => res.json());
+
+      setTodos(todos => todos.map(todo =>{
+        if (todo._id == data._id){
+          todo.complete = data.complete;
+        }
+
+        return todo;
+      }));
+  }
+
+  const deleteTodo = async id =>{
+    const data = await fetch(API_BASE + "/todo/delete/" + id, {
+      method: "DELETE"
+    }).then(res => res.json());
+
+      setTodos(todos => todos.filter(todo => todo._id !== data._id));   
+  }
+
   return (
     <div className="App">
       <h1>Welcome, Viewer</h1>
       <h4>Your Tasks</h4>
-      
+
       <div className="todos">
-        <div className="todo">
-          <div className="checkbox"></div>
+        {todos.map(todo => (
+          <div className={
+              "todo " + (todo.complete ? "is-complete" : "")
+              } key={todo._id} onClick={()=> completeTodo(todo.id)}>
+                <div className="checkbox"></div>
 
-          <div className="text">Get the bread</div>
+                <div className="text">{todo.text}</div>
 
-          <div className="delete-todo">X</div>
-        </div>
-        <div className="todo is-complete">
-          <div className="checkbox"></div>
-
-          <div className="text">Get the milk</div>
-
-          <div className="delete-todo">X</div>
-        </div>
+                <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>X</div>
+          </div>
+        ))}
       </div>
     </div>
   );
