88455580dd6b2dbf3b08a98e08b9da774b54dd95
John Bynum <iexclusiveproductions@gmail.com>
2022-11-29 20:08:11 -0500

    fixing minor bugs

---
+++ api/routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const path = require('path');
+// const apiRoutes = require('./api');
+
+// router.use('/api', apiRoutes);
+
+// serve up react front-end in production ****may have to uncomment this out 
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+// });
+
+module.exports = router;
