2df97d5182fad5af18e4551dd66a8ee167c51827
John Bynum <iexclusiveproductions@gmail.com>
2022-11-29 19:25:14 -0500

    Modified syntax for mutation and queries forms

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,25 @@
 import { gql } from '@apollo/client'; 
 
-export const CREATE_TASK = gql`
-mutation createTask()
-`
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!){
+    loginUser(email: $email, password: $password){
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+` ;
+
+export const ADD_USER = `gql
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username:$username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
